21d0f5971da3d5617a3a02ea4aa374a8febace77
sansantas <ortegaan@oregonstate.edu>
2022-03-06 14:50:56 -0800

    initial deployment operational

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('whee.')
+  res.send('ahhh.. muahahahahaaaa...')
 })
 
 app.listen(port, () => {

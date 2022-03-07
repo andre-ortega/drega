9e6b1522f3335ae5d31e2f9375a5a64ab99f2f8d
sansantas <ortegaan@oregonstate.edu>
2022-03-07 11:38:19 -0800

    changed landing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('ahhh.. muahahahahaaaa...')
+  res.send('Live.')
 })
 
 app.listen(port, () => {

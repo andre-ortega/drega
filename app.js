5f99fee4ec271b7f2c6d104227b19e8743bf0f6a
sansantas <ortegaan@oregonstate.edu>
2022-03-08 00:17:21 -0800

    void

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('<body style="background-color: #1d2021;"><div><meta charset="UTF-8"><p style="font-size:24px; text-align:center; color:#ebdbb2">You\'re crushing it, Shawnmigo.</p></div></body>')
+  res.send('<body style="background-color: #1d2021;"><div><meta charset="UTF-8"><p style="font-size:24px; text-align:center; color:#ebdbb2">[void]</p></div></body>')
 })
 
 app.listen(port, () => {

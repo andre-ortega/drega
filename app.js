55b8bae92dd5cc6c39735c8875bbf9cc54794e11
sansantas <ortegaan@oregonstate.edu>
2022-03-07 22:21:18 -0800

    shawn msg

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
-app.use('/static', express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('...')
+  res.send('<body style="background-color: #1d2021;"><div><meta charset="UTF-8"><p style="font-size:24px; text-align:center; color:#ebdbb2">You\'re crushing it, Shawnmigo.</p></div></body>')
 })
 
 app.listen(port, () => {

9cb9fbf99a296fb98679eef8d6bbcdf9a2b6d407
sansantas <ortegaan@oregonstate.edu>
2022-05-17 15:09:38 -0700

    added favicon and full width of body

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const ClrScr = require('./utils/ClrScr');
 ClrScr();
 
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 const router = express.Router();
 const session = require('express-session');
 const ejsMate = require('ejs-mate');
@@ -9,12 +11,13 @@ const ExpressError = require('./utils/ExpressError');
 
 const homeRoutes = require('./routes/home');
 
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, './public')));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.io')));
 
 app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));

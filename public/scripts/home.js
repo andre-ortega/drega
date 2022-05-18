26ca4a7cd79a22f315aafcfda3c4eacfee863fa2
sansantas <ortegaan@oregonstate.edu>
2022-05-17 17:34:30 -0700

    Disappearing main page

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,14 +1,25 @@
 console.log('herro');
 IDs = document.querySelectorAll('.toggle');
+portfolio = document.querySelector('.portfolio')
 
-const myScrollFunc = function() {
+
+const scrollStart = function() {
     let y = window.scrollY;
     if (y > 0) {
         for (element of IDs){
             element.classList.add('hide');
             element.classList.remove('show');
+            portfolio.classList.add('show');
+            document.body.classList.add('hideImg');
         }
+    } else {
+        for (element of IDs){
+            element.classList.add('show');
+            element.classList.remove('hide');
+            portfolio.classList.remove('show');
+            document.body.classList.remove('hideImg');
+        } 
     }
 }
 
-window.addEventListener("scroll", myScrollFunc);
+window.addEventListener("scroll", scrollStart);

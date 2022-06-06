7540709e3154500feac6994f0412ef4ebecf7542
sansantas <ortegaan@oregonstate.edu>
2022-06-06 12:54:03 -0700

    flash

--- controllers/home.js
+++ controllers/home.js
@@ -2,11 +2,15 @@
 
 // File where we are exporting specific functions
 module.exports.home = (req, res) => {
+    console.log("\n req.flash:")
+    console.log(req.flash);
     res.render('home', { messages: req.flash('success') });
 }
 
 // File where we are exporting specific functions
 module.exports.success = (req, res) => {
     req.flash('success', 'Successfully Sent Email');
+    console.log("\n req:")
+    console.log(req)
     res.redirect('/');
 }

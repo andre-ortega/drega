5943ada612d7ba5f7943d20cd765201461ea41cc
sansantas <ortegaan@oregonstate.edu>
2022-06-06 11:47:52 -0700

    debug messages

--- controllers/home.js
+++ controllers/home.js
@@ -7,6 +7,8 @@ module.exports.home = (req, res) => {
 
 // File where we are exporting specific functions
 module.exports.success = (req, res) => {
+    console.log('\nATTEMPTING..\n')
     req.flash('success', 'Successfully Sent Email');
     res.redirect('/');
+    console.log('\nATTEMPTED..\n')
 }
